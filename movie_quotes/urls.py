f7d027fe01597cbe2071b860b5c5d863cab44041
Brian Loveless <Brian.loveless1@yahoo.com>
2019-11-07 11:47:12 -0500

    redirect start to home page

--- movie_quotes/urls.py
+++ movie_quotes/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.quote_list, name='quote_list'),
+    path('', views.home, name='home'),
+    path('quotes', views.quote_list, name='quote_list'),
     path('quotes/<int:pk>', views.quote_detail, name='quote_detail'),
     path('quotes/new', views.quote_create, name='quote_create'),
     path('quotes/<int:pk>/edit', views.quote_edit, name='quote_edit'),
