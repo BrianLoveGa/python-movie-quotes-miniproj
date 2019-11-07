d296719d59d5e1e39a4af22c0f4cedb9fc0b5eff
Brian Loveless <Brian.loveless1@yahoo.com>
2019-11-07 16:18:56 -0500

    year range

--- movie_quotes/models.py
+++ movie_quotes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
@@ -6,7 +7,19 @@ from django.db import models
 
 class Movie(models.Model):
     movie_title = models.CharField(default="movie title", max_length=100, unique=True)
-    movie_release_year = models.IntegerField(max_length=4)
+    movie_release_year = models.IntegerField(
+                default=1980)
+    #             ,
+    #     validators=[
+    #         MaxValueValidator(2050),
+    #         MinValueValidator(1900)
+    #     ])
+    #  this works to limit the year entry but when implemented and and a year
+    #   higher than 2050 or lower than 1900 is entered it crashes the app 
+    #    instead of issuing a warning message or pop-up will research more later
+
+
+
     imbd_link = models.CharField(default="imdb link or url", max_length=200)
     movie_poster = models.CharField(default="link to poster or pic", max_length=200)
     def __str__(self):
