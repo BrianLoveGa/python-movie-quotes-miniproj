24f04391467f89c7441adb12ccff41b2d930d444
Brian Loveless <Brian.loveless1@yahoo.com>
2019-11-07 13:43:02 -0500

    css

--- movie_quotes/models.py
+++ movie_quotes/models.py
@@ -5,8 +5,8 @@ from django.db import models
 ## git add content
 
 class Movie(models.Model):
-    movie_title = models.CharField(default="movie title", max_length=100)
-    movie_release_year = models.CharField(default="year", max_length=10)
+    movie_title = models.CharField(default="movie title", max_length=100, unique=True)
+    movie_release_year = models.IntegerField(max_length=4)
     imbd_link = models.CharField(default="imdb link or url", max_length=200)
     movie_poster = models.CharField(default="link to poster or pic", max_length=200)
     def __str__(self):
